60c79d27b50c131a4e7251e5faf93514eb9772ab
IJ-Jany <146128361+IJ-Jany@users.noreply.github.com>
2025-07-31 02:00:57 +0600

    push-pop

---
+++ array2.js
@@ -0,0 +1,11 @@
+ const destinations = [ 'kaptai', 'barektila', 'saint-martin']
+
+ destinations.push('matita')
+ console.log(destinations)
+
+ destinations.push('nilgiri', 'lawachara')
+ console.log(destinations)
+
+ destinations.pop();
+ console.log(destinations)
+ 
